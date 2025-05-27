eee3ab94e18efcaf50f6488e5aac08ab55875343
José Antônio <85037819+dede6giu@users.noreply.github.com>
2025-05-27 16:08:16 -0300

    dev: skeleton actor

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,24 @@ use std::sync::mpsc;
 use std::collections::HashMap;
 use std::collections::HashSet;
 
+enum Actor {
+    ActorWCC,
+    ActorWCM,
+    ActorDSM,
+    ActorSWM,
+}
+enum ActorMessage {
+    MessageWCC,
+    MessageWCM,
+    MessageDSM,
+    MessageSWM,
+}
+
 struct ActorWCC {
-    receiver: mpsc::Receiver<WCCMessage>,
+    receiver: mpsc::Receiver<MessageWCC>,
     handle_dsm: ActorDSM,
 }
-enum WCCMessage {
+enum MessageWCC {
     RunWIC {
         ref_dsm: ActorDSM,
     },
@@ -14,29 +27,69 @@ enum WCCMessage {
         data_map: HashMap<String, Vec<String>>,
     },
 }
+impl ActorWCC {
+    fn new(
+        receiver: mpsc::Receiver<MessageWCC>,
+        handle_dsm: ActorDSM,
+    ) -> Self {
+        ActorWCC {
+            receiver,
+            handle_dsm,
+        }
+    }
+
+    fn message_handle(&mut self, msg: ActorMessage) {
+        match msg {
+            MessageWCC::RunWIC { ref_dsm } => {},
+            MessageWCC::Display { data_map } => {},
+            _ => {},
+        }
+    }
+}
 
 struct ActorDSM {
-    receiver: mpsc::Receiver<DSMMessage>,
+    receiver: mpsc::Receiver<MessageDSM>,
     handle_swm: ActorSWM,
     data_raw: String,
     data_done: Vec<Vec<String>>,
 }
-enum DSMMessage {
+enum MessageDSM {
     Init {
         path_file_input: String,
         ref_swm: ActorSWM,
     },
     SendKeys {
-        author: ActorWCC,
+        author: Actor,
     },
+}
+impl ActorDSM {
+    fn new(
+        receiver: mpsc::Receiver<MessageDSM>,
+        handle_swm: ActorSWM,
+    ) -> Self {
+        ActorDSM {
+            receiver,
+            handle_swm,
+            data_raw: String::new(),
+            data_done: Vec::new(),
+        }
+    }
+
+    fn message_handle(&mut self, msg: ActorMessage) {
+        match msg {
+            MessageDSM::Init { path_file_input, ref_swm } => {},
+            MessageDSM::SendKeys { author } => {},
+            _ => {},
+        }
+    }
 }
 
 struct ActorSWM {
-    receiver: mpsc::Receiver<SWMMessage>,
+    receiver: mpsc::Receiver<MessageSWM>,
     handle_wcm: ActorWCM,
     data_swlist: HashSet<String>,
 }
-enum SWMMessage {
+enum MessageSWM {
     Init {
         ref_wcm: ActorWCM,
     },
@@ -45,17 +98,62 @@ enum SWMMessage {
         phrase: String,
     },
 }
+impl ActorSWM {
+    fn new(
+        receiver: mpsc::Receiver<MessageSWM>,
+        handle_wcm: ActorWCM,
+    ) -> Self {
+        ActorSWM {
+            receiver,
+            handle_wcm,
+            data_swlist: Vec::new(),
+        }
+    }
+
+    fn message_handle(&mut self, msg: ActorMessage) {
+        match msg {
+            MessageSWM::Init { ref_wcm } => {},
+            MessageSWM::Filter { keyword, phrase } => {},
+            _ => {},
+        }
+    }
+}
 
 struct ActorWCM {
-    receiver: mpsc::Receiver<WCMMessage>,
+    receiver: mpsc::Receiver<MessageWCM>,
     data_map: HashMap<String, Vec<String>>
 }
-enum WCMMessage {
+enum MessageWCM {
     Keyword {
         keyword: String,
         phrase: String,
     },
     RequestWICMap {
-        author: ActorWCC,
+        author: Actor,
     },
+}
+impl ActorWCM {
+    fn new( receiver: mspc::Receiver<MessageWCM> ) -> Self {
+        ActorWCM { 
+            receiver, 
+            data_map: HashMap<String, Vec<String>>::new(), 
+        }
+    }
+    fn message_handle(&mut self, msg: ActorMessage) {
+        match msg {
+            MessageWCM::Keyword { keyword, phrase } => {},
+            MessageWCM::RequestWICMap { author } => {}
+            _ => {},
+        }
+    }
+}
+
+async fn startup_actor(mut actor: Actor) {
+    while let Some(msg) = actor.receiver.recv().await {
+        actor.handle_message(msg);
+    }
+}
+
+fn main() {
+
 }
