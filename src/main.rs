7938d5e54187b62d955ef6c9b808a1932a58d1cb
José Antônio <85037819+dede6giu@users.noreply.github.com>
2025-05-27 09:30:27 -0300

    dev: Basic Structure

---
+++ src/main.rs
@@ -0,0 +1,61 @@
+use std::sync::mpsc;
+
+use tokio::sync::{oneshot, mpsc};
+
+struct ActorWCC {
+    receiver: mpsc::Receiver<WCCMessage>,
+    handle_dsm: ActorDSM,
+}
+enum WCCMessage {
+    RunWIC {
+        ref_dsm: ActorDSM,
+    },
+    Display {
+        data_map: HashMap<String, Vec<String>>,
+    },
+}
+
+struct ActorDSM {
+    receiver: mpsc::Receiver<DSMMessage>,
+    handle_swm: ActorSWM,
+    data_raw: String,
+    data_done: Vec<Vec<String>>,
+}
+enum DSMMessage {
+    Init {
+        path_file_input: String,
+        ref_swm: ActorSWM,
+    },
+    SendKeys {
+        author: ActorWCC,
+    },
+}
+
+struct ActorSWM {
+    receiver: mpsc::Receiver<SWMMessage>,
+    handle_wcm: ActorWCM,
+    data_swlist: HashSet<String>,
+}
+enum SWMMessage {
+    Init {
+        ref_wcm: ActorWCM,
+    },
+    Filter {
+        keyword: String,
+        phrase: String,
+    },
+}
+
+struct ActorWCM {
+    receiver: mpsc::Receiver<WCMMessage>,
+    data_map: HashMap<String, Vec<String>>
+}
+enum WCMMessage {
+    Keyword {
+        keyword: String,
+        phrase: String,
+    },
+    RequestWICMap {
+        author: ActorWCC,
+    },
+}
