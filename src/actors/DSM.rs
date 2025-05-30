97197490199800d2f4aa6dfd3f43f343fd01a026
Henrique-zoo <morcelleshenrique@gmail.com>
2025-05-30 09:22:06 -0300

    filterDSM

--- src/actors/DSM.rs
+++ src/actors/DSM.rs
@@ -10,6 +10,7 @@ pub struct ActorDSM {
     ref_swm: Addr::<SWM::ActorSWM>,
     data_raw: String,
     sentences: Vec<String>,
+    filtered_sentences: Vec<String>,
 }
 impl ActorDSM {
     pub fn new(
@@ -19,6 +20,7 @@ impl ActorDSM {
             ref_swm: child_swm,
             data_raw: String::new(),
             sentences: Vec::new(),
+            filtered_sentences: Vec::new(),
         }
     }
 }
@@ -70,17 +72,17 @@ impl Handler<Setup> for ActorDSM {
     type Result = ResponseFuture<Result<bool, std::io::Error>>;
 
     fn handle(&mut self, msg: Setup, _ctx: &mut Context<Self>) -> Self::Result {
-        // `this` é uma cópia de `self`. Não depende, portanto, do seu lifetime, que é limitado, pois a posse de `self` só foi emprestada ao escopo da função.
-        let mut this = self.clone();
-
         // Lê o arquivo
-        this.data_raw = fs::read_to_string(msg.path_input).expect("Não foi possível ler o arquivo");
+        self.data_raw = fs::read_to_string(msg.path_input).expect("Não foi possível ler o arquivo");
         // Forma o vetor de Strings
-        this.sentences = extract_sentences(&self.data_raw);
+        self.sentences = extract_sentences(&self.data_raw);
+
+        // `this` é uma cópia de `self`. Logo, o this, tem posse sobre seus valores, diferente do self, que só tem um empréstimo dessa posse, o que nos impede de mover seus valores nesse escopo (em que só temos um empréstimo dos valores em self)
+        let this = self.clone();
 
 
         // O `async move` cria uma espécie de struct (o Future) em que os atributos são cada variável contida em seu corpo (os valores das variáveis são movidos para esses atributos)
-        // Por isso que precisamos do this, não poderíamos fazer o move de um valor emprestado (self)
+        // Por isso que precisamos do this. Não poderíamos fazer o move de um valor emprestado (self)
         Box::pin(async move {
             this.ref_swm.send(SWM::Setup::new(msg.path_stopwords)).await.unwrap()
         })
@@ -103,11 +105,7 @@ impl SendKeys {
 impl Handler<SendKeys> for ActorDSM {
     type Result = ResponseFuture<Result<HashMap<String, Vec<String>>, std::io::Error>>;
 
-    fn handle(
-        &mut self,
-        _msg: SendKeys,
-        _ctx: &mut Context<Self>
-    ) -> Self::Result {
+    fn handle(&mut self, _msg: SendKeys, _ctx: &mut Context<Self>) -> Self::Result {
         let this = self.clone();
 
         Box::pin(async move {
@@ -117,7 +115,19 @@ impl Handler<SendKeys> for ActorDSM {
             // - Esperar Result
             // - Verificar Ok()
             // - Próxima iteração
-            this.ref_swm.send(SWM::Filter::new()).await.unwrap();
+            for sentence in &this.sentences {
+                let result_filter = this.ref_swm.send(SWM::Filter::new(sentence)).await.unwrap();
+
+                match result_filter {
+                    Ok(_) => continue,
+                    Err(e) => {
+                        return Err(std::io::Error::new(
+                            std::io::ErrorKind::Other,
+                            format!("Erro ao aplicar filtro: {}", e),
+                        ));
+                    },
+                }
+            }
             
             // TODO
             // - Enviar Transmit(WCM::ReqWIC) para SWM
